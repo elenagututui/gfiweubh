4f37cbfd1e530d92bddb4f7a5317f8f4c69e0c07
Elena <144125010+elenagututui@users.noreply.github.com>
2023-09-24 20:56:58 +0300

    Add files via upload

---
+++ problema 4.acasa.py
@@ -0,0 +1,5 @@
+v=16
+greutate=2*v+8 #in kg
+inaltime=5*v+80 #in cm
+print(greutate, "kg")
+print(inaltime, "cm" )
---
+++ problema 5.acasa.py
@@ -0,0 +1,3 @@
+n=8
+print("Ion spune", n)
+print("Vasile răspunde", n-2,n-1,n,n+1,n+2)
